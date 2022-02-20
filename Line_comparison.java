a809088b303e7ac6a672ab220467d0dc1496c40c
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-20 15:37:21 +0530

    Line comparison usecase1 uploaded

--- Line_comparison.java
+++ Line_comparison.java
@@ -1,8 +1,8 @@
 		import java.util.Scanner;
 public class Line_comparison {
-    public int length (int a,int b,int c,int d)
+    public double length (int a,int b,int c,int d)
     {
-        int lenofline=a+b+c+d;
+        double lenofline=Math.sqrt(Math.pow((c-a),2) + Math.pow((d-b),2));
         return lenofline;
     }
    public static void main(String args[]){
@@ -18,7 +18,7 @@ public class Line_comparison {
       System.out.println("enter y2");
       int y2=input.nextInt();
       Line_comparison e=new Line_comparison();
-      int len=e.length(x1,y1,x2,y2);
+      double len=e.length(x1,y1,x2,y2);
       System.out.println("length is : "+len+"");
    }
 }
