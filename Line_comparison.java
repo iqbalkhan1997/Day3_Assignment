9092eaf0a5c4ebaa800848456a638407c2e2ce38
iqbalkhan1997 <pathaniqbalkhan54321@gmail.com>
2022-02-20 15:16:44 +0530

    worked on Line comparision usecase

--- Line_comparison.java
+++ Line_comparison.java
@@ -1,6 +1,25 @@
+		import java.util.Scanner;
 public class Line_comparison {
-	public static void main(String[] args){
-		System.out.println("Welcome to Line Comparison Computation Program");
+    public int length (int a,int b,int c,int d)
+    {
+        int lenofline=a+b+c+d;
+        return lenofline;
+    }
+   public static void main(String args[]){
 
-	}
+      System.out.println("Welcome to line comparison");
+      Scanner input=new Scanner(System.in);
+      System.out.println("enter x1");
+      int x1=input.nextInt();
+      System.out.println("enter y1");
+      int y1=input.nextInt();
+      System.out.println("enter x2");
+      int x2=input.nextInt();
+      System.out.println("enter y2");
+      int y2=input.nextInt();
+      Line_comparison e=new Line_comparison();
+      int len=e.length(x1,y1,x2,y2);
+      System.out.println("length is : "+len+"");
+   }
 }
+
